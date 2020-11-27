2af1088a00a150968547d0bf94b7e95d93dbdb6e
DmytroGnatiuk <dschum46@gmail.com>
2020-11-27 19:33:30 +0200

    bs03 has been changed

--- bs03.erl
+++ bs03.erl
@@ -1,29 +1,20 @@
 -module(bs03).
 -export([split/2]).
 
-split(Bin, Spl) ->
-    K = list_to_binary(Spl),
-    Mysize = bit_size(K),
-    Count = Mysize div 8,
-    lists:reverse(split(Bin, Spl, K, Mysize, Count, Count, <<>>, [])).
-split(<<X, Rest/binary>>, Spl, K, Mysize, Count, Count, Accbin, Acc)
-    when (bit_size(<<Rest/binary>>) == Count*8) ->
-        split(<<>>, Spl, K, Mysize, Count, Count, <<Accbin/binary, X, Rest/binary>>, Acc);
-split(<<X, Rest/binary>>, Spl, K, Mysize, Count, Count, Accbin, Acc) ->
-    B = <<X, Rest/binary>>,  
-    <<Y:Mysize, Rest2/binary>> = B,    
-    S = <<Y:Mysize>>,
-    if 
-        S == K ->
-            split(Rest, Spl, K, Mysize, Count, 1, <<>>, [Accbin | Acc]);
-        true -> 
-            split(Rest, Spl, K, Mysize, Count, Count, <<Accbin/binary, X>>, Acc)        
-    end;
-split(<<X, Rest/binary>>, Spl, K, Mysize, Count, N, Accbin, Acc) ->
-   split(Rest, Spl, K, Mysize, Count, N+1, <<>>, Acc);
-split(<<>>, Spl, K, Mysize, Count, Count, Accbin, Acc) ->
-    [Accbin|Acc].
 
+split(BinText, Sep) -> 
+    BinSep = list_to_binary(Sep),
+    Size = byte_size(BinSep),
+    split(BinText, BinSep, Size, <<>>, []).
+split(Bin, Sep, Size, Word, Acc) ->
+    case Bin of
+    <<Sep:Size/binary, Rest/binary>> ->
+    split(Rest, Sep, Size, <<>>, [Word|Acc]);
+    <<C/utf8, Rest/binary>> ->
+    split(Rest, Sep, Size, <<Word/binary, C/utf8>>, Acc);
+    <<>> ->
+    lists:reverse([Word|Acc])
+    end.
 
 
 %%BS03: Разделить строку на части, с явным указанием разделителя:
